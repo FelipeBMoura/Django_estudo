2a9c157598fb6f00eb1657355e07ac1b1f479568
Felipe B. de Moura <fittato@gmail.com>
2021-12-04 17:41:35 -0300

    Capítulo 18 finalizado.

---
+++ moviereviews-project/accounts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.contrib.auth.forms import UserCreationForm
+
+
+def signupaccount(request):
+    return render(request, 'signupaccount.html', {'form': UserCreationForm})
